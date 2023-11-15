3bd5d7101adbf5a56dc18c5f4abd9d08bd0441b8
jdnaranjo1411 <112138324+jdnaranjo1411@users.noreply.github.com>
2023-11-15 16:21:01 -0500

    cambio de tarefa a tarea

--- timeFlow/src/components/FakeAPI/data.js
+++ timeFlow/src/components/FakeAPI/data.js
@@ -7,13 +7,13 @@ const Eventos = [
         tasks: [
             {
                 id: 1,
-                title: 'Tarefa 1',
+                title: 'Tarea 1',
                 start: new Date(2021, 5, 1, 10, 0, 0),
                 end: new Date(2021, 5, 1, 10, 30, 0),
             }, 
             {
                 id: 2,
-                title: 'Tarefa 2',
+                title: 'Tarea 2',
                 start: new Date(2021, 5, 1, 10, 30, 0),
                 end: new Date(2021, 5, 1, 11, 0, 0),
             }
