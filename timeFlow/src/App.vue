ffa9665a69f40d1b9a2f89513d56eab6cc5f836a
jdnaranjo1411 <112138324+jdnaranjo1411@users.noreply.github.com>
2023-11-15 18:38:41 -0500

    El boton de inicio funciona :)

--- timeFlow/src/App.vue
+++ timeFlow/src/App.vue
@@ -1,17 +1,48 @@
 <template>
-    <div>
-        <!--Contenido de la app-->
-        <AddButtom @showPopup="showPopup"/>
-        <CreatePopUp></CreatePopUp>
-        
-    </div>
+     <div>
+    <!-- Contenido de tu aplicación -->
+    <AddButton @showPopup="showPopup" />
+    <CreatePopup :showPopup="popupVisible" @closePopup="closePopup" @createProject="createProject" />
+  </div>
 <RouterView/> 
 </template>
+
 <script>
 import { RouterView } from 'vue-router';
 import CreatePopUp from './components/Molecules/CreatePopUp.vue';
+import AddButton from '@/components/atoms/AddButton.vue';
+import CreatePopup from '@/components/molecules/CreatePopup.vue';
 
-
+export default{
+    data() {
+    return {
+      popupVisible: false,
+    };
+  },
+  methods: {
+    showPopup() {
+      this.popupVisible = true;
+    },
+    closePopup() {
+      this.popupVisible = false;
+    },
+    createProject(projectData) {
+      // Lógica para agregar el proyecto a Vuex
+      this.$store.dispatch('addProject', projectData);
+    },
+  },
+  computed: {
+    // Puedes usar este computed para obtener la lista de proyectos
+    projects() {
+      return this.$store.getters.getProjects;
+    },
+  },
+  components: {
+    AddButton,
+    CreatePopup,
+  },
+};
 </script>
+
 
 
