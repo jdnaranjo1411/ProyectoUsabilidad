0582a825f3d8080d4cd28913197d19c7b0064960
jdnaranjo1411 <112138324+jdnaranjo1411@users.noreply.github.com>
2023-11-18 21:20:52 -0500

    Se agregaron las moleculas y los atomos necesarios para las opciones de tipografia con errores

---
+++ timeFlow/src/components/Molecules/FontConfiguration.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="font-configuration">
+      <FontSelector @font-changed="changeFont" />
+      <FontSizeSelector @font-size-changed="changeFontSize" />
+    </div>
+  </template>
+  
+  <script>
+  import FontSelector from "@/components/atoms/FontSelector.vue";
+  import FontSizeSelector from "@/components/atoms/FontSizeSelector.vue";
+  
+  export default {
+    name: "FontConfiguration",
+    components: {
+      FontSelector,
+      FontSizeSelector,
+    },
+    methods: {
+      changeFont(selectedFont) {
+        this.$emit("font-changed", selectedFont);
+      },
+      changeFontSize(selectedFontSize) {
+        this.$emit("font-size-changed", selectedFontSize);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .font-configuration {
+    display: flex;
+    /* Otros estilos según sea necesario */
+  }
+  </style>
+  
--- timeFlow/src/components/Organisms/Header.vue
+++ timeFlow/src/components/Organisms/Header.vue
@@ -1,78 +1,77 @@
 <script>
-
 import OpcionesHeader from "@/components/Molecules/OpcionesHeader.vue";
 import HeaderHamburguer from "@/components/Atoms/Headerutil/HeaderHamburguer.vue";
-export default{
-    components:{
-        OpcionesHeader,
-        HeaderHamburguer
-        
+import HeaderOption from "@/components/Atoms/Headerutil/HeaderOption.vue";
+
+export default {
+  components: {
+    OpcionesHeader,
+    HeaderHamburguer,
+    HeaderOption,
+  },
+  data() {
+    return {
+      screenWidth: window.innerWidth,
+    };
+  },
+  mounted() {
+    window.addEventListener('resize', this.handleResize);
+  },
+  methods: {
+    handleResize() {
+      this.screenWidth = window.innerWidth;
     },
-    data(){
-        return{
-            screenWidth: window.innerWidth
-        }
-    },
-    mounted(){
-        window.addEventListener('resize', this.handleResize)
-    },
-    methods:{
-        handleResize(){
-            this.screenWidth = window.innerWidth;
-        }
-
-    }
-
-}
-
-
-
+  },
+};
 </script>
+
 <template>
-    <header>
-        <nav>
-            <HeaderHamburguer v-if="(screenWidth < 768)" @click = "$emit('Hamburguer-click')"></HeaderHamburguer>
-            <h1>
-                TIMEFLOW
-            </h1>
-            <OpcionesHeader v-if = "(screenWidth >= 768)" @button-clicked-settings= "$emit('button-clicked-settings-h')" @button-clicked-account = "$emit('button-clicked-account')"></OpcionesHeader>
-            
-        </nav>
-    </header>
+  <header>
+    <nav>
+      <HeaderHamburguer v-if="screenWidth < 768" @click="$emit('Hamburguer-click')" />
+      <h1>TIMEFLOW</h1>
+      <OpcionesHeader v-if="screenWidth >= 768" @button-clicked-settings="$emit('button-clicked-settings-h')" @button-clicked-account="$emit('button-clicked-account')" />
+      <HeaderOption v-if="screenWidth >= 768">
+        <template v-slot:icon>format_size</template>
+        <template v-slot:tooltip>Cambiar Tamaño de Letra</template>
+      </HeaderOption>
+      <HeaderOption v-if="screenWidth >= 768">
+        <template v-slot:icon>font_download</template>
+        <template v-slot:tooltip>Cambiar Fuente</template>
+      </HeaderOption>
+    </nav>
+  </header>
 </template>
+
 <style>
 header {
-    display: flex;
-    align-items: center;
-    height: 80px;
-    text-align: center;
-    width: 100%;
-    box-shadow: 0 0 10px 4px var(--color-shadow1);
-
+  display: flex;
+  align-items: center;
+  height: 80px;
+  text-align: center;
+  width: 100%;
+  box-shadow: 0 0 10px 4px var(--color-shadow1);
 }
 
 header nav {
-    display: flex;
-    height: 100%;
-    align-items: center;
+  display: flex;
+  height: 100%;
+  align-items: center;
 }
 
-
-
 .contenedorHamburguer .material-symbols-outlined {
-    font-size: 2rem;
+  font-size: 2rem;
 }
 
 @media (min-width: 768px) {
-    
-    header {
-        padding-left: 25px;
-        padding-right: 25px;
-    }
+  header {
+    padding-left: 25px;
+    padding-right: 25px;
+  }
 
-    header nav {
-        width: 100%;
-        justify-content: space-between;
-    }
+  header nav {
+    width: 100%;
+    justify-content: space-between;
+  }
 }
 </style>
